c9fbe433390cb712ee0ca72c40c9f5ed0cdfa338
Ameen Ahsan <ameenahsanma@gmail.com>
2022-09-19 23:51:07 +0530

    added config file

---
+++ rajdhani/constants.py
@@ -0,0 +1,11 @@
+"""This module contain constants and mappings for various parts of this app
+"""
+
+TICKET_CLASSES = {
+    "SL": "sleeper",
+    "3A": "third_ac",
+    "2A": "second_ac",
+    "1A": "first_ac",
+    "FC": "first_class",
+    "CC": "chair_car"
+}
